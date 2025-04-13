58427c61fce6e260d7d8f447e69ff29bcc9618e1
ZeYanLiow <bryan.liow.zy@gmail.com>
2025-04-13 15:23:03 +0100

    fix: sonarqube failing

--- src/test/java/com/example/reward_service/service/RewardServiceTest.java
+++ src/test/java/com/example/reward_service/service/RewardServiceTest.java
@@ -31,8 +31,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.*;
-
 
 class RewardServiceTest {
 
